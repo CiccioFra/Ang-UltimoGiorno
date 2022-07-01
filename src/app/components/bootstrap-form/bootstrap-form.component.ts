01af9b5fd7c70c44d2dcaef7c901466041fc7bb3
Francesco <mail>
2022-07-01 15:59:31 +0200

    boh

--- src/app/components/bootstrap-form/bootstrap-form.component.ts
+++ src/app/components/bootstrap-form/bootstrap-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-bootstrap-form',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bootstrap-form.component.scss']
 })
 export class BootstrapFormComponent implements OnInit {
+  @ViewChild('form') form!: ElementRef; // 1
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  save() {  // la richiamiamo nell'html
+    this.form.nativeElement.checkValidity();
+    this.form.nativeElement.classList.add('was-validated');
+  }
 }
